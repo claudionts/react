ae42a9b833fb9493605f60516b90e0556d1949bc
claudiomeditool <claudionts@gmail.com>
2018-06-02 18:34:48 -0300

    Connection Api

--- cdc-admin/src/App.js
+++ cdc-admin/src/App.js
@@ -1,8 +1,59 @@
 import React, { Component } from 'react';
 import './css/pure-min.css';
-import './css/side-menu.css'
+import './css/side-menu.css';
+import $ from 'jquery';
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {lista: [], nome:'', email:'', senha:''};
+    this.enviaForm = this.enviaForm.bind(this);
+    this.setNome = this.setNome.bind(this);
+    this.setEmail = this.setEmail.bind(this);
+    this.setSenha = this.setSenha.bind(this);
+  }
+
+  componentDidMount() {
+    $.ajax({
+      //url:"http://cdc-react.herokuapp.com/api/autores",
+      url: "http://cdc-react.herokuapp.com/api/autores",
+      dataType: 'json',
+      success: function(res){
+        this.setState({lista: res});
+      }.bind(this)
+    });
+  }
+
+  enviaForm(evento){
+    evento.preventDefault();
+    $.ajax({
+      url:'http://cdc-react.herokuapp.com/api/autores',
+      contentType:'application/json',
+      dataType:'json',
+      type:'post',
+      data:JSON.stringify({nome:this.state.nome,email:this.state.email,senha:this.state.senha}),
+      success: function(res){
+        this.setState({lista:res});
+      }.bind(this),
+      error: function(res){
+        console.log('erro')
+      }
+    });
+  }
+
+  setNome(evento){
+    this.setState({nome:evento.target.value});
+  }
+
+  setEmail(evento){
+    this.setState({email:evento.target.value});
+  }
+
+  setSenha(evento){
+    this.setState({senha:evento.target.value});
+  }
+
   render() {
     return (
       <div id="layout">
@@ -28,18 +79,19 @@ class App extends Component {
           </div>
           <div className="content" id="content">
             <div className="pure-form pure-form-aligned">
-              <form className="pure-form pure-form-aligned">
+            {/*onSubmit={this.enviaForm.bind(this)}*/}
+              <form className="pure-form pure-form-aligned" onSubmit={this.enviaForm} method="post">
                 <div className="pure-control-group">
                   <label htmlFor="nome">Nome</label>
-                  <input id="nome" type="text" name="nome" value="" />
+                  <input id="nome" type="text" name="nome" value={this.state.nome} onChange={this.setNome} />
                 </div>
                 <div className="pure-control-group">
                   <label htmlFor="email">Email</label>
-                  <input id="email" type="email" name="email" value="" />
+                  <input id="email" type="email" name="email" value={this.state.email} onChange={this.setEmail} />
                 </div>
                 <div className="pure-control-group">
                   <label htmlFor="senha">Senha</label>
-                  <input id="senha" type="password" name="senha" />
+                  <input id="senha" type="password" name="senha" value={this.state.senha} onChange={this.setSenha} />
                 </div>
                 <div className="pure-control-group">
                   <label></label>
@@ -57,10 +109,16 @@ class App extends Component {
                   </tr>
                 </thead>
                 <tbody>
-                  <tr>
-                    <td>Alberto</td>
-                    <td>alberto.souza@caelum.com.br</td>
-                  </tr>
+                  {
+                    this.state.lista.map((autor) => {
+                      return(
+                        <tr key={autor.id}>
+                          <td>{autor.nome}</td>
+                          <td>{autor.email}</td>
+                        </tr>
+                      )
+                    })
+                  }
                 </tbody>
               </table>
             </div>
