bcba91b1694575baf4c38de44e6189897be9e466
claudiomeditool <claudionts@gmail.com>
2018-06-03 19:11:14 -0300

    Implementing react-router-dom

---
+++ cdc-admin/src/componentes/Livro.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export default class Livro extends Component {
+	render() {
+		return (
+			<h4>Livros</h4>
+		);
+	}
+}
--- cdc-admin/src/index.js
+++ cdc-admin/src/index.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import App from './App';
+import AutorBox from './componentes/Autor';
+import Home from './componentes/Home';
+import Livro from './componentes/Livro';
 
 ReactDOM.render(
-    React.createElement(App), 
+    (<Router>
+        <App>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/autor" component={AutorBox} />
+                <Route path="/livro" component={Livro} />
+            </Switch>
+        </App>
+    </Router>),
     document.getElementById('root'));
 registerServiceWorker();
