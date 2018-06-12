e6d45c325addd2b87b2d8876412c01490c3665d9
claudiomeditool <claudionts@gmail.com>
2018-06-12 00:25:04 -0300

    Using APIRest with SpringBoot

--- instareact/src/componentes/Timeline.js
+++ instareact/src/componentes/Timeline.js
@@ -2,12 +2,26 @@ import React, { Component } from 'react';
 import Foto from './Foto';
 
 export default class Timeline extends Component {
+
+  constructor() {
+    super();
+    this.state = { fotos: [] };
+  }
+
+  componentDidMount() {
+    fetch('http://instalura-api.herokuapp.com/api/public/fotos/rafael')
+      .then(response => response.json())
+      .then(fotos => {
+        this.setState({ fotos: fotos });
+      });
+  }
+
   render() {
     return (
       <div className="fotos container">
-        <Foto />
-
-        <Foto />
+        {
+          this.state.fotos.map(foto => <Foto key={foto.id} foto={foto} />)
+        }
       </div>
     );
   }
